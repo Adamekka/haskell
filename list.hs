d7d5ae9aaceecc73ff950a79926ce42a20953c6a
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2024-10-22 09:59:29 +0200

    list

--- list.hs
+++ list.hs
@@ -1,3 +1,5 @@
+import GHC.IO.Exception (assertError)
+
 sumList :: [Int] -> Int
 sumList [] = 0
 sumList (x : xs) = x + sumList xs
@@ -52,6 +54,31 @@ reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' (x : xs) = reverse' xs ++ [x]
 
+take' :: Int -> [a] -> [a]
+take' 0 _ = []
+take' _ [] = []
+take' n (x : xs) = x : take' (n - 1) xs
+
+drop' :: Int -> [a] -> [a]
+drop' 0 a = a
+drop' n (_ : xs) = drop' (n - 1) xs
+
+minimum' :: (Ord a) => [a] -> Maybe a
+minimum' [] = Nothing
+minimum' [x] = Just x
+minimum' (x : y : xs)
+  | x < y = minimum' ([x] ++ xs)
+  | otherwise = minimum' ([y] ++ xs)
+
+divisors :: Int -> [Int]
+divisors 0 = []
+divisors n = tmp 1
+  where
+    tmp i
+      | i == n = [n]
+      | mod n i == 0 = i : tmp (i + 1)
+      | otherwise = tmp (i + 1)
+
 main :: IO ()
 main = do
   let res :: Int = sumList [5, 5]
@@ -63,4 +90,8 @@ main = do
   let res7 :: [Int] = combine [1, 2] [3, 4]
   let res8 :: Int = maxInt [1, 2, 3]
   let res9 :: [Int] = reverse' [1, 2, 3]
-  putStrLn $ show res ++ show res2 ++ show res3 ++ show res4 ++ show res5 ++ show res6 ++ show res7 ++ show res8 ++ show res9
+  let res10 :: [Int] = take' 2 [1, 2, 3]
+  let res11 :: [Int] = drop' 2 [1, 2, 3]
+  let res12 :: Maybe Int = minimum' [1, 3, 4, 0]
+  let res13 :: [Int] = divisors 32
+  putStrLn $ show res ++ show res2 ++ show res3 ++ show res4 ++ show res5 ++ show res6 ++ show res7 ++ show res8 ++ show res9 ++ show res10 ++ show res11 ++ show res12 ++ show res13
